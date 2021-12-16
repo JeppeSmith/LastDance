01a33264c02f8e35982ed7a9c8fc5019444adc4e
Albinhsn <albin.henningsson1@gmail.com>
2021-12-16 17:27:48 +0100

    First

---
+++ src/www/main.js
@@ -0,0 +1,132 @@
+let recipe = {}
+let recipes = []
+
+let file = {}
+let files = []
+
+async function postRecipe() {
+    try {
+        let response = await fetch('/recipes', (req, res), {
+            method: 'POST',
+            body: recipe
+        })
+        console.log("Posted Recipe");
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+async function deleteRecipe() {
+    try {
+        let response = await fetch(`/recipes/${id}`(req, res), {
+            method: 'DELETE'
+        });
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+async function postFile() {
+    try {
+        let response = await fetch('/uploads', (req, res), {
+            method: 'POST',
+            body: file
+        })
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+async function getFiles() {
+    try {
+        let response = await fetch('/files')
+        files = await response.json();
+        let html = document.querySelector('').files
+        html.innerHTML = ""
+        files.map(file => {
+            html +=
+                `
+            <div>
+            </div>
+        `
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function deleteFile(id) {
+    try {
+        let response = await fetch(`/files/${id}`)
+        console.log(response)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+async function getRecipeById(id) {
+    try {
+        let response = await fetch(`/recipes/${id}`)
+        recipe = await response.json();
+        let html = document.querySelector('');
+        html.innerHTML = `
+        <div>
+        
+        </div>
+    `
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+async function getRecipeByName(name) {
+    try {
+        let response = await fetch(`recipes/name/${name}`)
+        recipe = await response.json();
+        let html = document.querySelector('');
+        html.innerHTML = `
+        <div>
+        </div> 
+    `
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+async function getRecipes() {
+    try {
+        let response = await fetch('/recipes');
+        recipes = await response.json();
+        console.log(recipes)
+        let html = document.querySelector('.Returns');
+        html.innerHTML = '';
+        recipes.map(recipe => {
+            html.innerHTML += `
+            <div> 
+                <img src = '${recipe.image}'> 
+                <h2> ${recipe.name} </h2>
+                <p> ${recipe.description} </p>
+            </div>
+            `
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+async function updateRecipe(id) {
+    try {
+        let response = await fetch(`/recipes/${id}`, (req, res), {
+            method: 'PUT',
+            body: recipeState
+        })
+        console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
+}
